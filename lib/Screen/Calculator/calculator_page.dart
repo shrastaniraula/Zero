6c1a052fdc7e493ed0678759d8695530748ae0bd
shrastaniraula <shrastatsarhs58@gmail.com>
2024-01-20 01:18:35 +0545

    calc

--- lib/Screen/Calculator/calculator_page.dart
+++ lib/Screen/Calculator/calculator_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+// import 'package:circular/circular.dart';
+import 'package:zero/Widgets/calculator/circular_drag.dart';
 // import 'tips_screen.dart';
 
 class CarbonFootprintCalculator extends StatefulWidget {
@@ -16,6 +18,9 @@ class _CarbonFootprintCalculatorState extends State<CarbonFootprintCalculator> {
   double _meatConsumption = 0;
   double _plasticUsage = 0;
   double _electricityUsage = 0;
+  double _age = 0;
+
+  // double house = 0;
 
   // Define the variables that will store the carbon footprint calculations
   double _carCarbonFootprint = 0;
@@ -89,122 +94,212 @@ class _CarbonFootprintCalculatorState extends State<CarbonFootprintCalculator> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.dark,
-      ),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Carbon Footprint Calculator'),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text(
-                'Enter your information to calculate your carbon footprint:',
-                style: TextStyle(
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.bold,
-                ),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Container(
+              width: MediaQuery.sizeOf(context).width * 0.5,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white,
+                boxShadow: <BoxShadow>[
+                  BoxShadow(
+                      color: const Color.fromARGB(255, 185, 185, 185),
+                      offset: Offset(1, 1),
+                      blurRadius: 5.0),
+                ],
               ),
-              SizedBox(height: 16.0),
-              TextFormField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: 'Car mileage (miles per gallon)',
-                ),
-                onChanged: (value) {
-                  _carMileage = double.tryParse(value) ?? 0;
-                },
+              margin: const EdgeInsets.all(60),
+              padding: const EdgeInsets.all(50),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const Text(
+                    'Enter your information to calculate your carbon footprint:',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 16.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Column(
+                        children: [
+                          const Text(
+                            "Car mileage \n(miles per gallon)",
+                            textAlign: TextAlign.center,
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          CircularSlide(
+                            setValue: (val) {
+                              setState(() {
+                                _carMileage = val;
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          const Text(
+                            "Car usage time \n(hours per week)",
+                            textAlign: TextAlign.center,
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          CircularSlide(
+                            setValue: (val) {
+                              setState(() {
+                                _carUsageTime = val;
+                              });
+                            },
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Column(
+                        children: [
+                          const Text(
+                            "Flights per year\n",
+                            textAlign: TextAlign.center,
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          CircularSlide(
+                            setValue: (val) {
+                              setState(() {
+                                _flightsPerYear = val;
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          const Text(
+                            "Public transport usage\n(hrs/week)",
+                            textAlign: TextAlign.center,
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          CircularSlide(
+                            setValue: (val) {
+                              setState(() {
+                                _publicTransportUsageTime = val;
+                              });
+                            },
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Column(
+                        children: [
+                          const Text(
+                            "Meat consumption \n(pounds per week)",
+                            textAlign: TextAlign.center,
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          CircularSlide(
+                            setValue: (val) {
+                              setState(() {
+                                _meatConsumption = val;
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          const Text(
+                            "Plastic usage \n(pounds per week)",
+                            textAlign: TextAlign.center,
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          CircularSlide(
+                            setValue: (val) {
+                              setState(() {
+                                _plasticUsage = val;
+                              });
+                            },
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Column(
+                        children: [
+                          const Text(
+                            "Electricity Usage \n(Kilowatts per month)",
+                            textAlign: TextAlign.center,
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          CircularSlide(
+                            setValue: (val) {
+                              setState(() {
+                                _electricityUsage = val;
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          const Text(
+                            "Your Age\n",
+                            textAlign: TextAlign.center,
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          CircularSlide(
+                            setValue: (val) {
+                              setState(() {
+                                _age = val;
+                              });
+                            },
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  SizedBox(height: 16.0),
+                  ElevatedButton(
+                    onPressed: _calculateCarbonFootprint,
+                    child: Text('Calculate'),
+                  ),
+                  SizedBox(height: 16.0),
+                ],
               ),
-              TextFormField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: 'Car usage time (hours per week)',
-                ),
-                onChanged: (value) {
-                  _carUsageTime = double.tryParse(value) ?? 0;
-                },
-              ),
-              TextFormField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: 'Public transport usage time (hours per week)',
-                ),
-                onChanged: (value) {
-                  _publicTransportUsageTime = double.tryParse(value) ?? 0;
-                },
-              ),
-              TextFormField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: 'Flights per year',
-                ),
-                onChanged: (value) {
-                  _flightsPerYear = double.tryParse(value) ?? 0;
-                },
-              ),
-              TextFormField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: 'Meat consumption (pounds per week)',
-                ),
-                onChanged: (value) {
-                  _meatConsumption = double.tryParse(value) ?? 0;
-                },
-              ),
-              TextFormField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: 'Plastic usage (pounds per week)',
-                ),
-                onChanged: (value) {
-                  _plasticUsage = double.tryParse(value) ?? 0;
-                },
-              ),
-              TextFormField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: 'Electricity usage (kilowatt-hours per month)',
-                ),
-                onChanged: (value) {
-                  _electricityUsage = double.tryParse(value) ?? 0;
-                },
-              ),
-              SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: _calculateCarbonFootprint,
-                child: Text('Calculate'),
-              ),
-              SizedBox(height: 16.0),
-              Text(
-                'Your carbon footprint is:',
-                style: TextStyle(
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 8.0),
-              Text(
-                '${_totalCarbonFootprint.toStringAsFixed(2)} tons of CO2 per year',
-                style: TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.pink,
-                ),
-              ),
-              // ElevatedButton(
-              //   onPressed: () {
-              //     Navigator.push(
-              //       context,
-              //       MaterialPageRoute(
-              //           builder: (context) => CarbonFootprintTipsScreen()),
-              //     );
-              //   },
-              //   child: Text('Reduce Carbon Emissions'),
-              // ),
-            ],
+            ),
           ),
         ),
       ),
